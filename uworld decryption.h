b12da19f83d72a9bc89a0cd79ae7ab0f88a34373
Zero <mexopaypal@gmail.com>
2026-03-19 19:22:02 +0000

    Refactor decryptUWorld function to DecryptUWorld
    Updated decryption logic and function name for clarity.

--- uworld decryption.h
+++ uworld decryption.h
@@ -1,4 +1,10 @@
-inline uint64_t decryptUWorld(uint64_t world)
+inline uint64_t DecryptUWorld(uint64_t world)
 {
-    return ~std::rotl(world^ 0x97363D09ULL, 50);
+    return ~std::rotl(world, 32) ^ 0x97F199673D1F48C6uLL;
 }
+
+//example usage
+uintptr_t EncryptedUWorld = Driver->read<uintptr_t>(Driver->BaseAddress + offsets::UWorld);
+uintptr_t DecryptedUWorld = DecryptUWorld(EncryptedUWorld);
+
+uintptr_t UWorld = DecryptedUWorld;
