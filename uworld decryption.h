f4d7c66c894f5e4fe5ebc6860e638e5efa1932e3
Zero <mexopaypal@gmail.com>
2026-04-17 00:08:41 +0100

    Fix DecryptUworld function return type and syntax

--- uworld decryption.h
+++ uworld decryption.h
@@ -1,6 +1,6 @@
 static inline uintptr_t DecryptUworld(uint64_t v)
 {
-    return v?(std::rotl(v,5)^0x165D340ULL)*0x24042FF8A5B240D7ULL-0x235CE507FE35F6B5ULL:0;
+    return (uintptr_t)((_rotl64(v, 5) ^ 0x165D340ULL) * 0x24042FF8A5B240D7ULL - 0x235CE507FE35F6B5ULL);
 }
 
 //example usage
